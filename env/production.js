b0cb4a2b9b8c76af165a6903981871907e5f0b05
Emily Malec Brown <emilymalecbrown@gmail.com>
2016-12-24 22:41:42 -0500

    Update production.js

--- env/production.js
+++ env/production.js
@@ -1,5 +1,6 @@
 module.exports = {
   USER_ID: process.env.API_TOKEN,
   API_TOKEN:  process.env.API_TOKEN,
-  API_SECRET: process.env.API_SECRET
+  API_SECRET: process.env.API_SECRET, 
+  PHONE: process.env.PHONE
 }

0ed684f3a52f5f0b4d653d28d292b4f91986929a
emilymalecbrown <emilymalecbrown@gmail.com>
2016-12-31 13:58:52 -0500

    fail more informatively when costco sends bad html

--- scraper.js
+++ scraper.js
@@ -23,21 +23,29 @@ const task = () => {
   let url = 'http://www.costco.com/Kirkland-Signature-Four-Piece-Urethane-Cover-Golf-Ball,-2-dozen.product.100310467.html';
   request(url, function(error, response, html){
 
-    var $ = cheerio.load(html);
+    let $ = cheerio.load(html);
 
     if(error){
       throw new Error(error);
     }
 
-    // if there is an out of stock button - don't send text, else alert!
-
-    if ( $('#product-page #product-details #ctas #add-to-cart input[type="button"]')['0'].attribs.value === 'Out of Stock') {
-      alreadyAvailable = false;
-      console.log("still out of stock");
+    let productStockStatus = $('#product-page #product-details #ctas #add-to-cart input[type="button"]')['0'];
+    // catch for cases where Costco page doesn't load as expected
+    // more informative errors!
+    if (productStockStatus === 'undefined') {
+      throw new Error("Didn't get HTML this time");
     } else {
-      if (alreadyAvailable === false) {
-        sendMessage();
-        alreadyAvailable = true;
+
+      // if there is an out of stock button - don't send text, else alert!
+
+      if ( productStockStatus.attribs.value === 'Out of Stock') {
+        alreadyAvailable = false;
+        console.log("still out of stock");
+      } else {
+        if (alreadyAvailable === false) {
+          sendMessage();
+          alreadyAvailable = true;
+        }
       }
     }
 
